5fc8391f94e6b63c26829a35d326de2170ab7e6d
Abraham8661 <abrahamedet9@gmail.com>
2023-12-20 14:08:53 +0100

    Second Commit

--- BuddyBlog/settings.py
+++ BuddyBlog/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--vcpgd1v4(r*3h%48z$$^(ec@iokl#d$o$&h=n@(3!5+349$9k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "buddy-blog-0e4cf9783a16.herokuapp.com", "127.0.0.1"
+]
 
 
 # Application definition
@@ -46,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -149,3 +152,5 @@ MEDIA_URL = "/files/"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.User"
+
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
