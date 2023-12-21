16def455b1063dfd85ae8428359ab8a611754022
Abraham8661 <abrahamedet9@gmail.com>
2023-12-21 16:17:25 +0100

    Database Resolve

--- BuddyBlog/settings.py
+++ BuddyBlog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY'),
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "buddy-blog-0e4cf9783a16.herokuapp.com", "127.0.0.1"
@@ -85,24 +85,24 @@ WSGI_APPLICATION = 'BuddyBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': os.environ.get('BUDDY_USER', 'default_user'),
-        'PASSWORD': os.environ.get('BUDDY_PASSWORD', 'default_password'),
-        'HOST': os.environ.get('BUDDY_HOST', 'localhost'),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'postgres',
+#        'USER': os.environ.get('BUDDY_USER', 'default_user'),
+#        'PASSWORD': os.environ.get('BUDDY_PASSWORD', 'default_password'),
+#        'HOST': os.environ.get('BUDDY_HOST', 'localhost'),
+#        'PORT': '5432',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
