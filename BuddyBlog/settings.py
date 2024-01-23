8612607bc849bdd8c77e10ced1d73ec26a991125
Abraham8661 <abrahamedet9@gmail.com>
2024-01-23 15:54:00 +0100

    Database Change Fix2

--- BuddyBlog/settings.py
+++ BuddyBlog/settings.py
@@ -85,23 +85,13 @@ WSGI_APPLICATION = 'BuddyBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
-  'default': {
-      'ENGINE': 'django.db.backends.postgresql',
-      'NAME': 'buddy_blog',
-      'USER': 'postgres',
-      'PASSWORD': 'Mr.Abraham1@',
-      'HOST': os.environ.get('BUDDY_HOST', 'localhost'),
-      'PORT': '5432',
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 #DATABASES = {
 #    'default': {
